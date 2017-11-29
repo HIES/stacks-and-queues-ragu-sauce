0b713391266315716a091a4b1e65c218f2c8360b
cbuzzhasAdog <31040225+cbuzzhasAdog@users.noreply.github.com>
2017-11-29 10:12:08 -0500

    Stack Done

--- Stack.java
+++ Stack.java
@@ -1,20 +1,78 @@
+import java.util.EmptyStackException;
 public class Stack<E>
 {
+	private Object[] stack;
+	private int top = -1;
+	private int curArraySize = 0;
+
+
 	public Stack()
-	{}
+	{
+		stack = new Object[10];
+		curArraySize = 10;
+	}
 
 	public void push(E item)
-	{}
+	{
+		if (stack[curArraySize-1]!=null){
+			Object[] temp = new Object[stack.length*2] ;
+			for (int i = 0; i < stack.length; i++) {
+				temp[i] = stack[i];
+			}
+			stack = temp;
+			curArraySize = stack.length;
 
-	public E pop()
-	{return null;}
+			stack[top]=item;
+			top++;
+		}
+		else{
+			top++;
+			stack[top] = item;
+		}
+	}
+
+	public E pop() throws EmptyStackException
+	{
+		if (isEmpty()){
+			throw new EmptyStackException();
+		}
+		else{
+
+			Object temp = stack[top];
+			stack[top] = null;
+			top--;
+			return (E) temp;
+		}
+
+	}
 
 	public E peek()
-	{return null;}
+	{
+		if (isEmpty()){
+			throw new EmptyStackException();
+		}
+		else{
+
+
+			return (E) stack[top];
+		}
+	}
 
 	public boolean isEmpty()
-	{return false;}
+	{if (size()==0)
+		return true;
+	else
+		return false;
+	}
+
 
 	public int size()
-	{return -1;}
+	{
+		if (top == -1)
+			return 0;
+		else{
+			return top + 1;
+		}
+
+	}
 }
